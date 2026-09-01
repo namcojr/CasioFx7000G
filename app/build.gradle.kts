646e69ff9b4c66c69320e28a4789a9e91c0cffaf
NaMcO <namcojr@gmail.com>
2026-09-01 09:42:47 +0100

    Build update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.retro.fx7000g"
         minSdk = 24
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 25
+        versionName = "2.5.0"
     }
 
     // Name the built APK e.g. "Casio FX-7000G v1.0.0.apk"
